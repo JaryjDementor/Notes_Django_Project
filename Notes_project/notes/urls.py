c0eb1f949bf54029b79860268c7b1bbee526413e
JaryjDementor <90262843+JaryjDementor@users.noreply.github.com>
2022-02-05 16:20:31 +0100

    create system register & login

--- Notes_project/notes/urls.py
+++ Notes_project/notes/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', hallo),
+    path('', first_page),
+    path('/home', hallo_world, name='homepage'),
+    path("register", register_request, name="register"),
+    path("login", login_request, name="login")
+
 ]
