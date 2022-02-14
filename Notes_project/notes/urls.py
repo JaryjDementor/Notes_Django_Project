74a3512841a343dd66af7211aea79d70f20c1d2e
JaryjDementor <90262843+JaryjDementor@users.noreply.github.com>
2022-02-14 17:25:14 +0100

    make frontend detailnote

--- Notes_project/notes/urls.py
+++ Notes_project/notes/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', first_page),
+    path('', first_page,name='first_page'),
     path("register", register_request, name="register"),
-    path("login", login_request, name="login")
+    path("login", login_request, name="login"),
+    path("logout", logout_request, name="logout"),
 
 ]
--- Notes_project/notes/views.py
+++ Notes_project/notes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import NewUserForm
-from django.contrib.auth import login, authenticate,models
+from django.contrib.auth import login, authenticate, models, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -39,3 +39,8 @@ def login_request(request):
             messages.error(request,"Invalid username or password.")
     form = AuthenticationForm()
     return render(request=request, template_name="notes/login.html", context={"login_form":form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect("first_page")
