b33c8f2e2577424ec00e731f4fe91cc727dab992
JaryjDementor <90262843+JaryjDementor@users.noreply.github.com>
2022-04-07 15:07:11 +0200

    create func reset password

--- Notes_project/accounts/urls.py
+++ Notes_project/accounts/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from .views import login_request, first_page, register_request, logout_request
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
+from .views import login_request, first_page, register_request, logout_request, resset_password_request, check_mail
 
 urlpatterns = [
     path("", first_page, name="first_page"),
     path("register", register_request, name="register"),
     path("login", login_request, name="login"),
     path("logout", logout_request, name="logout"),
+    path("reset_password", resset_password_request, name="reset"),
+    path("succesful_send_mail", check_mail, name="succesful_send"),
+    path("reset_password_confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name = "accounts/password_confirm.html",success_url=reverse_lazy('login')), name="password_confirm"),
 ]
