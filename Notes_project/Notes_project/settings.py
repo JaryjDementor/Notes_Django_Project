6a458188ca6dc2f907de61d82b2d5aa86c8a0c07
JaryjDementor <90262843+JaryjDementor@users.noreply.github.com>
2022-04-13 16:41:41 +0200

    upd settin debag

--- Notes_project/Notes_project/settings.py
+++ Notes_project/Notes_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-56thd^n5qmg244i3vrik$n%yt*l3=*qfi)xzw@^bmgcsfn+&h9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
