12338065f0b1fd8fb310a707949f043d7e54f9e6
JaryjDementor <90262843+JaryjDementor@users.noreply.github.com>
2022-04-26 14:49:49 +0200

    create notes.tests.test_models

--- Notes_project/notes/models.py
+++ Notes_project/notes/models.py
@@ -6,8 +6,8 @@ from datetime import datetime
 
 
 class Notebook(Model):
-    name_notebook = CharField("Title", max_length=100)
-    iduser = CharField("idUser", max_length=5)
+    name_notebook = CharField("name_notebook", max_length=100)
+    iduser = CharField("iduser", max_length=5)
 
     class Meta:
         verbose_name = "News"
@@ -21,11 +21,11 @@ class Notebook(Model):
 
 
 class Note(Model):
-    title = CharField("Title", max_length=100)
-    text = CharField("Your note", max_length=1000)
+    title = CharField("title", max_length=100)
+    text = CharField("text", max_length=1000)
     data = DateTimeField(default=datetime.now())
-    iduser = CharField("idUser", max_length=5)
-    id_notebook = CharField("idUser", max_length=5)
+    iduser = CharField("iduser", max_length=5)
+    id_notebook = CharField("id_notebook", max_length=5)
 
     class Meta:
         verbose_name = "News"
