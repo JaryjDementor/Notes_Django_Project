3eae4b821fb39ddbbae20a13265ba5fb98895880
JaryjDementor <jaryjdementor@gmail.com>
2022-05-23 16:50:49 +0200

    upd all templates

--- Notes_project/Notes_project/settings.py
+++ Notes_project/Notes_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-56thd^n5qmg244i3vrik$n%yt*l3=*qfi)xzw@^bmgcsfn+&h9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
